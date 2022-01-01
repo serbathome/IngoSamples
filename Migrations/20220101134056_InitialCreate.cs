d521b2f5a4fea745d01a7d9273180537f54fd44e
Sergey Biryukov <serbathome@gmail.com>
2022-01-01 17:01:55 +0300

    Added database support

---
+++ Migrations/20220101134056_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sampleapp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    TodoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CompletionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    isCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    isStarted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.TodoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
---
+++ Models/TodoModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace sampleapp.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Todo>? Todos { get; set; }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=tcp:ingodbsrv.database.windows.net,1433;Initial Catalog=ingodb;Persist Security Info=False;User ID=ingodbadmin;Password=W9qxsriQ5;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+    }
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        public string Title { get; set; } = "";
+        public string Details { get; set; } = "";
+        public DateTime CreationDate { get; set; } = DateTime.Today;
+        public DateTime CompletionDate { get; set; } = DateTime.MaxValue;
+        public bool isCompleted { get; set; } = false;
+        public bool isStarted { get; set; } = false;
+    }
+}
